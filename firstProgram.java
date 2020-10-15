56999ee98b6505643f33b67ea9f4fa6e4b7e1264
akoluraws-devops <72905357+akoluraws-devops@users.noreply.github.com>
2020-10-15 01:18:53 -0400

    Update firstProgram.java
    Added one line wishes

--- firstProgram.java
+++ firstProgram.java
@@ -1,4 +1,5 @@
 public class Simple{
 public static void main(String args[]){
 System.out.println("hello Amit, this is first java program");
+  System.out.println("COngratulations! looks good...");
 }}
